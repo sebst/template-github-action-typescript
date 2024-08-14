e86ec167fbff87c6e897ae51c9091cdb680a8244
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-14 13:43:47 +0200

    Pre-final

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as core from '@actions/core'
+
+async function run(): Promise<void> {
+  const nameToGreet = core.getInput('who-to-greet')
+  core.info(`Hello, ${nameToGreet}!`)
+  core.setOutput('greeting', `Hello, ${nameToGreet}!`)
+}
+
+run().catch(error => {
+  core.setFailed((error as Error).message)
+})
